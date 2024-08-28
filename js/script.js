b8352938250cafac8d3960c2411f2808dcd14598
armandotruglio <armando.truglio1@gmail.com>
2024-08-28 19:24:50 +0200

    Created Object array with the given data

---
+++ js/script.js
@@ -0,0 +1,40 @@
+//Creo un array di object
+
+const team = [
+    {
+        firstName: 'Wayne',
+        lastName: 'Barnett',
+        role: 'Founder & CEO',
+        photo: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        firstName: 'Angela',
+        lastName: 'Carrol',
+        role: 'Chied Editor',
+        photo: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        firstName: 'Walter',
+        lastName: 'Gordon',
+        role: 'Office Manager',
+        photo: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        firstName: 'Angela',
+        lastName: 'Lopez',
+        role: 'Social Media Manager',
+        photo: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        firstName: 'Scott',
+        lastName: 'Estrada',
+        role: 'Developer',
+        photo: 'scott-estrada-developer.jpg'
+    },
+    {
+        firstName: 'Barbara',
+        lastName: 'Ramos',
+        role: 'Graphic Designer',
+        photo: 'barbara-ramos-graphic-designer.jpg '
+    }
+]
